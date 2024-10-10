e6792b26734924d372a20a0b7fbae4e66200e4b8
Raquel Marie <raqueldelamer@gmail.com>
2024-10-10 18:12:39 -0500

    update counter/index.js

--- next-js/src/pages/counter/index.js
+++ next-js/src/pages/counter/index.js
@@ -1,25 +1,22 @@
 import { useState } from "react";
 
-
 export default function CounterPage(){
 
 const [count, setCount] = useState(0);
   // Don't declare state with a regular variable
-  // let count = 10; 
 
-  // This should increment count by 1
 function handleClick() {
-    // count++;
-    setCount( count + 1 );
+    
+    setCount( count + 1 ); // (count + );
     // alert(`You have ${count} cats.`)
 }
 return (<div>
     <h1>Lets Count Cats!</h1>
     <div>
     <h3>You have {count} cats.</h3> 
-    <button onClick={ handleClick }>
+        <button onClick={ handleClick }>
         Click for cats!
-    </button>
+        </button>
     </div>
     </div>);
 
