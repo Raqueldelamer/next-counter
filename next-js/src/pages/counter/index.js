7fdfd27502a8cf95078f54c84fae279ef6e34f70
Raquel Marie <raqueldelamer@gmail.com>
2024-10-12 14:22:01 -0500

    complete counter/index.js counter

--- next-js/src/pages/counter/index.js
+++ next-js/src/pages/counter/index.js
@@ -4,20 +4,44 @@ export default function CounterPage(){
 
 const [count, setCount] = useState(0);
   // Don't declare state with a regular variable
+const [number] = useState(1);
 
 function handleClick() {
     
-    setCount( count + 1 ); // (count + );
+    setCount( count + 2 ); // (count + );
     // alert(`You have ${count} cats.`)
 }
+
+function handleIncrement() {
+    setCount(count + number);
+}
+
+function handleDecrement() {
+    if (count - number >= 0) {
+    setCount(count - number);
+    }
+}
+
+function handleReset() {
+    setCount(0);
+}
+
 return (<div>
     <h1>Lets Count Cats!</h1>
     <div>
     <h3>You have {count} cats.</h3> 
+
         <button onClick={ handleClick }>
-        Click for cats!
-        </button>
+        Click to double cats!
+        </button><br />
+        
+        <button onClick={ handleIncrement }>more =^.^= </button><br />
+
+        <button onClick={ handleDecrement }>less =^.^= </button><br />
+
+        <button onClick={ handleReset }>no =^.^= </button><br />
     </div>
     </div>);
 
 }
+
